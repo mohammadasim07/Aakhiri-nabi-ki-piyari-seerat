d39ff25ab0e08489c45fd02011e37a08eea9dd89
mohammadasim07 <asim.lpu07@gmail.com>
2026-09-14 14:57:32 +0530

    feat: implement Vercel Analytics

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { Analytics } from '@vercel/analytics/react'
 import './index.css'
 import App from './App.jsx'
 import { registerServiceWorker } from './utils/offlineManager'
@@ -9,5 +10,6 @@ registerServiceWorker();
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
+    <Analytics />
   </StrictMode>,
 )
